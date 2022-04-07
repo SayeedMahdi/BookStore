1e4a1eadf24925d1a4e29c72f6616b3b50c853e8
SayeedMahdi <sayeedmahdimousavi789@gmail.com>
2022-04-07 11:10:18 +0430

    new changes

--- routes/routes.js
+++ routes/routes.js
@@ -4,6 +4,9 @@ router.get("/home",(req,res)=>{
     res.send("welcome home");
 }
 );
+router.route("/",(req,res)=>{
+    res.send("<h1>hello world</h1>")
+})
 router.route("/Book").post(controler.insertbook).get(controler.Allbooks);
 router.route("/Book/search").get(controler.search);
 
