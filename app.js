4f3d437df73f02e5b6640511744acce61116246a
SayeedMahdi <sayeedmahdimousavi789@gmail.com>
2022-04-07 11:53:37 +0430

    local host error

--- app.js
+++ app.js
@@ -4,18 +4,19 @@ const app = express();
 const notFound = require("./middlewares/404not-exist");
 const DB = require("./dbConection/Db");
 const route = require("./routes/routes");
-const erroHandle = require("./middlewares/errorHandle")
-
+const erroHandle = require("./middlewares/errorHandle");
 
 require("dotenv").config();
-app.use(erroHandle)
+app.use(erroHandle);
 app.use(express.json());
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*")
+}) 
 app.use("/api/v1", route);
 //midlle wares
 app.use(express.json());
 const port = process.env.PORT || 4000;
-const mongoose_URI =
-  process.env.MONGO_URI ;
+const mongoose_URI = process.env.MONGO_URI;
 const start = async () => {
   try {
     await DB(mongoose_URI);
