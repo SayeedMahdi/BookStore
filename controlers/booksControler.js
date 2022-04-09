428feb9f868903625a8839afb840b51cff755450
SayeedMahdi <sayeedmahdimousavi789@gmail.com>
2022-04-09 15:10:56 +0430

    all data is connected

--- controlers/booksControler.js
+++ controlers/booksControler.js
@@ -18,9 +18,7 @@ const Allbooks = asyncWrapper(async (req, res) => {
 });
 const search = asyncWrapper(async (req, res) => {
   //this is the object indepentent
-  var { value } = req.query.data;
-  
-  
+  var  value  = req.query;
   const result = await modelbook.find({
     $or: [
       { name: { $regex: value, $options: "i" } },
@@ -30,10 +28,20 @@ const search = asyncWrapper(async (req, res) => {
       { author: { $regex: value, $options: "i" } }
     ],
   });
-  res.status(200).json({ resultis: result });
+  res.status(200).json( result );
 });
+// delet tasks 
+const delet = asyncWrapper(async (req,res)=>{
+  const id = req.query.id;
+  const result = await modelbook.findOneAndDelete({_id:id});
+  if(!result){
+    res.status(404);
+  }
+  res.status(200).send(result);
+})
 module.exports = {
   insertbook,
   Allbooks,
   search,
+  delet
 };
