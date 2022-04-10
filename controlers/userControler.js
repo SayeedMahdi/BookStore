d3e38287ade8b635d152d984f2d836766c30a7c8
SayeedMahdi <sayeedmahdimousavi789@gmail.com>
2022-04-10 14:12:39 +0430

    added

--- controlers/userControler.js
+++ controlers/userControler.js
@@ -3,14 +3,17 @@ const jwt = require("jsonwebtoken");
 const middlewares = require("../middlewares/async");
 const validator = require("validator");
 const bcrypt = require("bcrypt");
-const salt = bcrypt.genSaltSync(10);
+const gen = process.env.gen;
+const salt = bcrypt.genSaltSync(gen);
 
 const insertUser = middlewares(async (req, res) => {
     const value = req.body;
     console.log(value);
   const email = req.body.email;
   const password = req.body.password;
+  console.log("email:  password:",email,password);
   const hashOne = await bcrypt.hash(password, salt);
+  console.log(hashOne);
   const secret = process.env.secret;
   const token = jwt.sign({ email, password }, secret);
   const valid = validator.isEmail(email);
