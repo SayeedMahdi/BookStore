9a0ce83bb3f07ef1ccc44bc167a4a7beade2b6f7
SayeedMahdi <sayeedmahdimousavi789@gmail.com>
2022-04-20 08:55:06 +0430

    new insertion

--- routes/routes.js
+++ routes/routes.js
@@ -14,4 +14,7 @@ router.route("/Book/search").get(BookControler.search);
 router.route("/User").post(userConteroler.insertUser)
 router.route("/User/login").post(userConteroler.login);
 router.route("/buyBook").post(authControl,BookControler.buyBook)
+router.get("/download",(req,res) =>{
+  res.download("../first home work.pdf","dowload.pdf");
+})
 module.exports = router;
