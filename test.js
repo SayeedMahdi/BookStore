313ef97327d3a648f1ed401545ea9275f2702698
SayeedMahdi <sayeedmahdimousavi789@gmail.com>
2022-04-30 08:48:22 +0430

    new changes

--- test.js
+++ test.js
@@ -1,11 +1,9 @@
-const fs = require('fs')
- 
-const fileStream = fs.createWriteStream('man.txt');
- 
-fileStream.write('This is the first line!'); 
-fileStream.write('This is the second line!');
-fileStream.end();
-let call= (err, data)=>{
-    console.log(data);
-}
-fs.readFile("./man.txt","utf-8",call);
+const ProgressBar = require('');
+
+const bar = new ProgressBar(':bar', { total: 10 });
+const timer = setInterval(() => {
+  bar.tick();
+  if (bar.complete) {
+    clearInterval(timer);
+  }
+}, 100);
